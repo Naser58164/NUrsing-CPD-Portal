07431298d1dc39a1a75b2a567cd47aee9f8839df
Naser58164 <n.r.a.s.58164@gmail.com>
2026-06-15 11:19:43 +0400

    Add files via upload

---
+++ prisma.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  earlyAccess: true,
+  schema: "./prisma/schema.prisma",
+  output: "./node_modules/.prisma/client",
+});
